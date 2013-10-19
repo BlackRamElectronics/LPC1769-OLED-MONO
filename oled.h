c3cda96c36cd981243a86227b3f46bdf72eade92
Reuben Morgan <reubenmorgan@blackramelectronics.com>
2013-10-19 19:45:03 +0100

    removed unused files

--- oled.h
+++
@@ -1,7 +0,0 @@
-void init_oled_display (void);
-
-#define HOME    0x1e    /* Home cursor */
-#define CLS     0x1a    /* Clear screen */
-#define ESC     0x1b    /* ESCAPE ( ^Z) */
-#define CR      0x0d    /* carriage return */
-#define LF      0x0a    /* Line feed */
